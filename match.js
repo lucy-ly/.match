4fb5b039774f0a3351a06eff900df5c0f9af037e
Lucy Ly <lucyly.student@careerdevs.com>
2019-05-26 18:40:14 +0000

    Applying the .match() method to extract the word coding

---
+++ match.js
@@ -0,0 +1,9 @@
+// Applying the .match() method to extract the word coding.
+
+// let extractStr = "Extract the word 'coding' from this string.";
+// let codingRegex = /change/; // Change this line
+// let result = extractStr; // Change this line
+
+let extractStr = "Extract the word 'coding' from this string.";
+let codingRegex = /coding/; 
+let result = extractStr.match(codingRegex); 
